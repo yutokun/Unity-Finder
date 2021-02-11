63695c04530856c670dd23e12594a0520792d2cf
yutoVR <yuto.fushimi@icloud.com>
2021-02-11 21:03:27 +0900

    First commit

---
+++ UnityFinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace UnityFinder
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\Program Files\Unity Hub\Unity Hub.exe",
+                Arguments = "-- --headless editors --installed",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            var process = new Process { StartInfo = startInfo };
+            process.Start();
+            var output = process.StandardOutput.ReadToEnd();
+            return output.Contains(args[0]) ? 0 : 1;
+        }
+    }
+}
